a2d2700a893b8058794f3308555e7c26eedb7ecf
sigurdurhelga <sigurdurhel@ru.is>
2020-02-13 11:19:49 +0000

    selfplay working I think

--- hlc_evaluator/selfplay.py
+++ hlc_evaluator/selfplay.py
@@ -1,16 +1,22 @@
 from monte_carlo.mcts import MCTS
+from monte_carlo.mctsnode import Node
 from game_environments.play_chess.playchess import PlayChess
-from game_environments.breakthrough.breakthrough import BTBoard
+from game_environments.breakthrough.breakthrough import BTBoard, config as BTconfig
 
 from collections import namedtuple
+import numpy as np
 
 game_setup = namedtuple("gamesetup", "game_class initial_state")
 
-BREAKTHROUGH_INITIAL_STATE = [
-    [2, 2, 2],
-    [0, 0, 0],
-    [1, 1, 1]
-]
+BREAKTHROUGH_INITIAL_STATE = (np.array([
+                  [BTconfig.BLACK,BTconfig.BLACK,BTconfig.BLACK,BTconfig.BLACK],
+                  [BTconfig.BLACK,BTconfig.BLACK,BTconfig.BLACK,BTconfig.BLACK],
+                  [BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY],
+                  [BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY],
+                  [BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY],
+                  [BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY,BTconfig.EMPTY],
+                  [BTconfig.WHITE,BTconfig.WHITE,BTconfig.WHITE,BTconfig.WHITE],
+                  [BTconfig.WHITE,BTconfig.WHITE,BTconfig.WHITE,BTconfig.WHITE]]), BTconfig.WHITE)
 
 CHESS_INITIAL_STATE = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
@@ -19,7 +25,15 @@ GAME = {
     "chess": game_setup(PlayChess, CHESS_INITIAL_STATE),
 }
 
-def play_game(white_think, black_think):
+selected_game = GAME["breakthrough"]
+initial_state = Node(selected_game.game_class(*selected_game.initial_state), "START")
+
+
+whiteplayer = None
+blackplayer = None
+
+def play_game(white_think, black_think, verbose=False):
+    global whiteplayer, blackplayer, selected_game
     """
     Function that plays a game between two players
     using monte carlo tree search, white_think and
@@ -27,32 +41,64 @@ def play_game(white_think, black_think):
     each player does
     """
     print("playgame started with {} {}".format(white_think, black_think))
-    whiteplayer = MCTS()
-    blackplayer = MCTS(negamaxing=True)
+    # initial node (root)
+    curr_node = Node(selected_game.game_class(*selected_game.initial_state), "START")
+    if whiteplayer:
+        whiteplayer.set_node(curr_node)
+        blackplayer.set_node(curr_node)
+    else:
+        whiteplayer = MCTS(curr_node)
 
-    selected_game = GAME["breakthrough"]
-
-    board = selected_game.game_class(selected_game.initial_state, 1)
     while True:
-        for _ in range(white_think):
-            whiteplayer.do_rollout(board)
-        board = whiteplayer.choose_child(board)
-        # print("===================")
-        # board.print_board()
-        # print("===================")
-        if board.is_terminal():
+        if verbose:
+            print("===================")
+            whiteplayer.root.gamestate.print_board()
+        whiteplayer.rollout(white_think)
+        whiteplayer.move_to_best_child()
+        if verbose:
+            print("did action: ",whiteplayer.root.action)
+            whiteplayer.root.gamestate.print_board()
+            print("===================")
+        if whiteplayer.root.gamestate.is_terminal():
+            print("terminal after whitemove")
             break
 
-        for _ in range(black_think):
-            blackplayer.do_rollout(board)
-        board = blackplayer.choose_child(board)
-        print("===================")
-        board.print_board()
-        print("===================")
-        if board.terminal:
-            break
-    return board.reward()
+        if not blackplayer:
+            blackplayer = MCTS(Node(whiteplayer.root.gamestate, whiteplayer.root.action), negamaxing=True)
+        else:
+            blackplayer.move_to_child(whiteplayer.root.action)
 
-for j in range(24, 25):
-    for k in range(1, 10):
-        print(play_game(j, k))
+        if verbose:
+            print("===================")
+            blackplayer.root.gamestate.print_board()
+        blackplayer.rollout(black_think)
+        blackplayer.move_to_best_child()
+        if verbose:
+            print("did action: ",blackplayer.root.action)
+            blackplayer.root.gamestate.print_board()
+            print("===================")
+        if blackplayer.root.gamestate.is_terminal():
+            print("terminal after blackmove")
+            break
+        whiteplayer.move_to_child(blackplayer.root.action)
+
+    #print("END RESULTS WHITE")
+    #print(whiteplayer.Ns)
+    #print(whiteplayer.Qs)
+    #print("END RESULTS BLACK")
+    #print(blackplayer.Ns)
+    #print(blackplayer.Qs)
+    return curr_node.gamestate.reward()
+wins = {
+    -1: 0,
+    0: 0,
+    1: 0
+}
+for i in range(3):
+    wins[play_game(80, 1)] += 1
+    print(wins)
+    print("INFO")
+    print("Whiteplayer initial state info", whiteplayer.Qs[initial_state], whiteplayer.Ns[initial_state])
+    print("blackplayer initial state info", blackplayer.Qs[initial_state], blackplayer.Ns[initial_state])
+    print("END RESULTS BLACK")
+print(wins)
